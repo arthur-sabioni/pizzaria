a7c34f14ae454602a6e820c9e66698cbba1a72f2
MariaIzamara <mariaizamaracoutinho@gmail.com>
2022-04-11 11:54:41 -0300

    address added

--- frontend/src/pages/Register/Register.jsx
+++ frontend/src/pages/Register/Register.jsx
@@ -1,17 +1,65 @@
-import React from 'react';
-import { Button, makeStyles, TextField } from '@material-ui/core';
+import React, { useState } from 'react';
+import { Button, makeStyles, MenuItem, TextField } from '@material-ui/core';
+
+// posteriormente esses bairros serão trazidos do banco de dados;
+const districts = [
+  {
+    value: 'NA',
+    label: 'Selecione',
+  },
+  {
+    value: 'NG',
+    label: 'Nova Gameleira',
+  },
+  {
+    value: 'NS',
+    label: 'Nova Suiça',
+  },
+  {
+    value: 'C',
+    label: 'Centro',
+  },
+  {
+    value: 'S',
+    label: 'Savassi',
+  },
+];
 
 const Register = () => {
-  const { container, title, form, field, button } = useStyles();
+  const { container, title, form, personalData, address, field, button } = useStyles();
+
+  const [district, setDistrict] = useState(districts[0].value);
 
   return (
     <div className={container}>
       <div className={title}>Cadastro</div>
       <div className={form}>
-        <TextField className={field} id="text-field-name" variant="outlined" label="Nome" />
-        <TextField className={field} id="text-field-cpf" variant="outlined" label="CPF" />
-        <TextField className={field} id="text-field-email" variant="outlined" label="E-mail" />
-        <TextField className={field} id="text-field-telephone" variant="outlined" label="Telefone" />
+        <div className={personalData}>
+          <TextField className={field} id="text-field-name" variant="outlined" label="Nome" />
+          <TextField className={field} id="text-field-cpf" variant="outlined" label="CPF" />
+          <TextField className={field} id="text-field-email" variant="outlined" label="E-mail" />
+          <TextField className={field} id="text-field-telephone" variant="outlined" label="Telefone" />
+        </div>
+        <div className={address}>
+          <TextField className={field} id="text-field-cep" variant="outlined" label="CEP" helperText="Somente são permitidos endereços na cidade de Belo Horizonte" />
+          <TextField
+            select
+            id="select-district"
+            variant="outlined"
+            label="Bairro"
+            value={district}
+            onChange={e => setDistrict(e.target.value)}
+          >
+            {districts.map(d => (
+              <MenuItem key={d.value} value={d.value}>
+                {d.label}
+              </MenuItem>
+            ))}
+          </TextField>
+          <TextField className={field} id="text-field-street" variant="outlined" label="Rua" />
+          <TextField className={field} id="text-field-number" variant="outlined" label="Número" />
+          <TextField className={field} id="text-field-complement" variant="outlined" label="Complemento" />
+        </div>
       </div>
       <div className={button}>
         <Button variant="contained">Cadastrar</Button>
@@ -33,8 +81,20 @@ const useStyles = makeStyles({
     display: 'flex',
     flexDirection: 'column',
   },
+  personalData: {
+    padding: 16,
+    marginBottom: 24,
+    border: 'solid 1px gray',
+    borderRadius: 4,
+  },
+  address: {
+    padding: 16,
+    marginBottom: 24,
+    border: 'solid 1px gray',
+    borderRadius: 4,
+  },
   field: {
-    marginBottom: 16,
+
   },
   button: {
     display: 'flex',
